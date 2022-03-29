491b03306984cf43af4ecd45de4b4d1bf9db8528
Joseph Chan <josephchan36@gmail.com>
2022-03-29 12:33:32 -0700

    updated

--- scripts/firebaseAPI_TEAM99.js.js
+++ scripts/firebaseAPI_TEAM99.js.js
@@ -2,12 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+        apiKey: "AIzaSyDIodtS0ae5rTvEML3qly34mlnt2ZWg-zs",
+        authDomain: "fir-comp1800-a3f83.firebaseapp.com",
+        projectId: "fir-comp1800-a3f83",
+        storageBucket: "fir-comp1800-a3f83.appspot.com",
+        messagingSenderId: "19704281715",
+        appId: "1:19704281715:web:013bb4a6365f108c724806",
+        measurementId: "G-DTBRRYWEX6"
 };
 
 //--------------------------------------------
